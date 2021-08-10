348962b7c3ecfcd5528a66d4552dc76d3cb8507d
Roger Camargo <huogerac@gmail.com>
2021-08-10 20:33:44 -0300

    Update the docker version

--- elementaryos5/02_docker.sh
+++ elementaryos5/02_docker.sh
@@ -14,6 +14,6 @@ sudo gpasswd -a $USER docker
 
 # Docker compose
 # https://docs.docker.com/compose/install/
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
